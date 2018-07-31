f9d8dc6b1436bb54888c980db9fbb34cca59456e
Marten Stockenberg <MStockenberg@googlemail.com>
2018-07-31 15:28:04 +0200

    updated inital setup

--- www/app/configs/PageAction.php
+++ www/app/configs/PageAction.php
@@ -9,9 +9,7 @@
 namespace sae\app\configs;
 
 
-use sae\app\controllers\HomeController;
 use sae\app\controllers\LoginController;
-use sae\app\controllers\UserController;
 
 class PageAction
 {
@@ -26,40 +24,6 @@ class PageAction
                 ],
             ],
         ],
-        'home' => [
-            'class' => HomeController::class,
-            'actions' => [
-                'logout' => [
-                    'allowed' => [],
-                    'method' => 'logout'
-                ]
-            ],
-        ],
-        'edit-users' => [
-            'class' => UserController::class,
-            'actions' => [
-                'default' => [
-                    'allowed' => [],
-                    'method' => 'init'
-                ],
-                'delete' => [
-                    'allowed' => [ADMIN],
-                    'method' => 'delete'
-                ],
-                'create' => [
-                    'allowed' => [ADMIN],
-                    'method' => 'create'
-                ],
-                'edit' => [
-                    'allowed' => [ADMIN],
-                    'method' => 'edit'
-                ],
-                'update' => [
-                    'allowed' => [ADMIN],
-                    'method' => 'update'
-                ]
-            ],
-        ]
     ];
 
 }
